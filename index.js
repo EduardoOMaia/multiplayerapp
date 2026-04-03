714ea4244f9989e9586dc8d1be93a290ff89f14c
Eduardo Maia <117035001+EduardoOMaia@users.noreply.github.com>
2026-04-02 23:29:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,117 +1,70 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const ytdl = require("ytdl-core");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json());
 
 const io = new Server(server, {
   cors: { origin: "*" }
 });
 
-app.use(express.static("public"));
-
 let rooms = {};
 
-function createRoom() {
-  return Math.random().toString(36).substring(2, 7);
-}
-
-/* 🔥 AUDIO STREAM */
-app.get("/audio", async (req, res) => {
-  try {
-    const url = req.query.url;
-
-    if (!ytdl.validateURL(url)) {
-      return res.status(400).send("URL inválida");
-    }
-
-    const info = await ytdl.getInfo(url);
-    const format = ytdl.chooseFormat(info.formats, {
-      quality: "highestaudio",
-      filter: "audioonly"
-    });
-
-    res.setHeader("Content-Type", format.mimeType);
-
-    ytdl(url, {
-      format: format,
-      highWaterMark: 1 << 25
-    }).pipe(res);
-
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Erro no áudio");
-  }
-});
-
-/* 🔥 SOCKET */
 io.on("connection", (socket) => {
 
-  let currentRoom = null;
+  let room = null;
 
-  socket.on("create-room", () => {
-    socket.emit("room-created", createRoom());
-  });
-
-  socket.on("join-room", (roomId) => {
+  socket.on("join", (roomId) => {
+    room = roomId;
+    socket.join(roomId);
 
     if (!rooms[roomId]) {
       rooms[roomId] = {
-        id: null,
-        queue: []
+        url: "",
+        isPlaying: false,
+        time: 0
       };
     }
 
-    currentRoom = roomId;
-    socket.join(roomId);
-
-    socket.emit("queue", rooms[roomId].queue);
-  });
-
-  socket.on("load", (data) => {
-    if (!currentRoom) return;
-
-    rooms[currentRoom].id = data.id;
-    io.to(currentRoom).emit("load", data);
-  });
-
-  socket.on("add-queue", (video) => {
-    if (!currentRoom) return;
-
-    rooms[currentRoom].queue.push(video);
-    io.to(currentRoom).emit("queue", rooms[currentRoom].queue);
-  });
-
-  socket.on("next", () => {
-    if (!currentRoom) return;
-
-    const room = rooms[currentRoom];
-
-    if (room.queue.length > 0) {
-      const next = room.queue.shift();
-      room.id = next.id;
-
-      io.to(currentRoom).emit("load", next);
-      io.to(currentRoom).emit("queue", room.queue);
-    }
+    socket.emit("sync", rooms[roomId]);
   });
 
   socket.on("play", (time) => {
-    socket.to(currentRoom).emit("play", time);
+    if (!room) return;
+    rooms[room].isPlaying = true;
+    rooms[room].time = time;
+
+    socket.to(room).emit("play", time);
   });
 
   socket.on("pause", (time) => {
-    socket.to(currentRoom).emit("pause", time);
+    if (!room) return;
+    rooms[room].isPlaying = false;
+    rooms[room].time = time;
+
+    socket.to(room).emit("pause", time);
   });
 
-  socket.on("chat-message", (data) => {
-    io.to(currentRoom).emit("chat-message", data);
+  socket.on("load", (url) => {
+    if (!room) return;
+
+    rooms[room].url = url;
+    rooms[room].time = 0;
+
+    io.to(room).emit("load", url);
   });
 
 });
 
+app.get("/", (req, res) => {
+  res.send("Servidor rodando");
+});
+
 server.listen(3000, () => {
-  console.log("🔥 servidor rodando");
+  console.log("🔥 Servidor rodando na porta 3000");
 });
